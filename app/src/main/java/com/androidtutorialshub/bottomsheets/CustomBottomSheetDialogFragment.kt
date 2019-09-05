fae5a624884be70620cdd357d77a052c6b4ce11d
m.metwally@arabia-it.com <m.metwally@arabia-it.com>
2019-09-05 17:41:04 +0200

    adjustsize dialogfragment, STATE_EXPANDED bottomsheet

--- app/src/main/java/com/androidtutorialshub/bottomsheets/CustomBottomSheetDialogFragment.kt
+++ app/src/main/java/com/androidtutorialshub/bottomsheets/CustomBottomSheetDialogFragment.kt
@@ -4,12 +4,52 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.RelativeLayout
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.android.synthetic.main.content_dialog_bottom_sheet.*
+import android.widget.FrameLayout
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import android.content.DialogInterface
 
-class CustomBottomSheetDialogFragment : BottomSheetDialogFragment() {
+
+
+class CustomBottomSheetDialogFragment : BottomSheetDialogFragment(){
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.content_dialog_bottom_sheet, container, false)
+        //Here is our bottomsheet dialog customization
+        initDialogListeners()
+
+        val view = inflater.inflate(R.layout.content_dialog_bottom_sheet, container, false)
+        return view
     }
+
+    private fun initDialogListeners() {
+        dialog?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+        dialog.setOnShowListener { dialog ->
+            //BottomSheetDialog wrapInBottomSheet() method
+            val bottomSheet = (dialog as BottomSheetDialog).findViewById(com.google.android.material.R.id.design_bottom_sheet) as FrameLayout?
+            BottomSheetBehavior.from(bottomSheet).state = BottomSheetBehavior.STATE_EXPANDED
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initviews()
+
+    }
+    private fun initviews() {
+        bottomSheetSubmitButton.setOnClickListener {
+            val text = bottomSheetInput.text.toString()
+            Toast.makeText(context, "submit :: " + text, Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+
 
 
 }
--- app/src/main/java/com/androidtutorialshub/bottomsheets/MainActivity.kt
+++ app/src/main/java/com/androidtutorialshub/bottomsheets/MainActivity.kt
@@ -1,114 +1,19 @@
 package com.androidtutorialshub.bottomsheets
 
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
-
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.main.content_main.*
 
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-    // BottomSheetBehavior variable
-    private var bottomSheetBehavior: BottomSheetBehavior<*>? = null
-    // TextView variable
-    private var bottomSheetHeading: TextView? = null
-    // Button variables
-    private var expandBottomSheetButton: Button? = null
-    private var collapseBottomSheetButton: Button? = null
-    private var hideBottomSheetButton: Button? = null
-    private var showBottomSheetDialogButton: Button? = null
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        initViews()
         initListeners()
-
-
     }
 
-    /**
-     * method to initialize the views
-     */
-    private fun initViews() {
-        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
-        setSupportActionBar(toolbar)
-
-        bottomSheetBehavior = BottomSheetBehavior.from(findViewById<View>(R.id.bottomSheetLayout))
-        bottomSheetHeading = findViewById<View>(R.id.bottomSheetHeading) as TextView
-
-        expandBottomSheetButton = findViewById<View>(R.id.expand_bottom_sheet_button) as Button
-        collapseBottomSheetButton = findViewById<View>(R.id.collapse_bottom_sheet_button) as Button
-        hideBottomSheetButton = findViewById<View>(R.id.hide_bottom_sheet_button) as Button
-        showBottomSheetDialogButton = findViewById<View>(R.id.show_bottom_sheet_dialog_button) as Button
-
-
-    }
-
-
-    /**
-     * method to initialize the listeners
-     */
     private fun initListeners() {
-        // register the listener for button click
-        expandBottomSheetButton!!.setOnClickListener(this)
-        collapseBottomSheetButton!!.setOnClickListener(this)
-        hideBottomSheetButton!!.setOnClickListener(this)
-        showBottomSheetDialogButton!!.setOnClickListener(this)
-
-        // Capturing the callbacks for bottom sheet
-        bottomSheetBehavior!!.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
-            override fun onStateChanged(bottomSheet: View, newState: Int) {
-
-                if (newState == BottomSheetBehavior.STATE_EXPANDED) {
-                    bottomSheetHeading!!.text = getString(R.string.text_collapse_me)
-                } else {
-                    bottomSheetHeading!!.text = getString(R.string.text_expand_me)
-                }
-
-                // Check Logs to see how bottom sheets behaves
-                when (newState) {
-                    BottomSheetBehavior.STATE_COLLAPSED -> Log.e("Bottom Sheet Behaviour", "STATE_COLLAPSED")
-                    BottomSheetBehavior.STATE_DRAGGING -> Log.e("Bottom Sheet Behaviour", "STATE_DRAGGING")
-                    BottomSheetBehavior.STATE_EXPANDED -> Log.e("Bottom Sheet Behaviour", "STATE_EXPANDED")
-                    BottomSheetBehavior.STATE_HIDDEN -> Log.e("Bottom Sheet Behaviour", "STATE_HIDDEN")
-                    BottomSheetBehavior.STATE_SETTLING -> Log.e("Bottom Sheet Behaviour", "STATE_SETTLING")
-                }
-            }
-
-
-            override fun onSlide(bottomSheet: View, slideOffset: Float) {
-
-            }
-        })
-
-
-    }
-
-    /**
-     * onClick Listener to capture button click
-     *
-     * @param v
-     */
-    override fun onClick(v: View) {
-        when (v.id) {
-            R.id.collapse_bottom_sheet_button ->
-                // Collapsing the bottom sheet
-                bottomSheetBehavior!!.setState(BottomSheetBehavior.STATE_COLLAPSED)
-            R.id.expand_bottom_sheet_button ->
-                // Expanding the bottom sheet
-                bottomSheetBehavior!!.setState(BottomSheetBehavior.STATE_EXPANDED)
-            R.id.hide_bottom_sheet_button ->
-                // Hiding the bottom sheet
-                bottomSheetBehavior!!.setState(BottomSheetBehavior.STATE_HIDDEN)
-            R.id.show_bottom_sheet_dialog_button ->
-                // Opening the Dialog Bottom Sheet
-                CustomBottomSheetDialogFragment().show(supportFragmentManager, "Dialog")
+        show_bottom_sheet_dialog_button.setOnClickListener { it ->
+            CustomBottomSheetDialogFragment().show(supportFragmentManager, "Dialog")
         }
     }
 }
