f34ac71bd7c8de1aaf445e845a301e3e484d8635
m.metwally@arabia-it.com <m.metwally@arabia-it.com>
2019-09-05 10:55:00 +0200

    migrate to kotlin , androidx

---
+++ app/src/main/java/com/androidtutorialshub/bottomsheets/CustomBottomSheetDialogFragment.kt
@@ -0,0 +1,15 @@
+package com.androidtutorialshub.bottomsheets
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class CustomBottomSheetDialogFragment : BottomSheetDialogFragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.content_dialog_bottom_sheet, container, false)
+    }
+
+
+}
